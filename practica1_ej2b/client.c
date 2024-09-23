e37aebfb0f896ab2c99d4303255dfe3ca8bc1e60
JCRigol <juan.rigol.uni@gmail.com>
2024-09-23 02:40:02 -0300

    Add files via upload

---
+++ practica1_ej2b/client.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>  // Para strlen y otras funciones de cadenas
+#include <unistd.h>  // Para read, write y otras funciones de E/S
+#include <stdlib.h>  // Para exit y otras funciones estándar
+#include <math.h>
+#include <zlib.h> // For CRC32
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    const uint16_t comm_size = 32733;
+
+    if (argc < 4) {
+       fprintf(stderr,"usage %s hostname port potencia\n", argv[0]);
+       exit(1);
+    }
+
+    // TOMA EL NÚMERO DE PUERTO DE LOS ARGUMENTOS
+    portno = atoi(argv[2]);
+	
+    // CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXIÓN
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+    // SOCK_STREAM - TIPO DE SOCKET 
+	
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+	
+    // TOMA LA DIRECCIÓN DEL SERVIDOR DE LOS ARGUMENTOS
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    // LIMPIA LA ESTRUCTURA serv_addr
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+	
+    // COPIA LA DIRECCIÓN IP Y EL PUERTO DEL SERVIDOR A LA ESTRUCTURA DEL SOCKET
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+	
+    // DESCRIPTOR - DIRECCIÓN - TAMAÑO DIRECCIÓN
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+
+    // ---- MENSAJE A ENVIAR ----
+    int potencia = atoi(argv[3]);
+    int buff_size = pow(10,potencia);
+    char buffer[buff_size];
+    memset(buffer, 'A', buff_size);
+    buffer[buff_size - 1] = '\0';  // Null-terminate the buffer for printf
+
+    // ---- PACKAGE NECESSARY INFO INTO FIRST COMM ----
+
+    // ---- CALCULATE N# OF COMMS NECESSARY ----
+    uint16_t nr_of_comms = (buff_size + comm_size - 1) / comm_size;
+
+    int fc_buff_size, cp_buff_size;
+    if (buff_size < comm_size) {
+        fc_buff_size = buff_size + 2 + 2; //2 bytes of nr_of_comms, 2 bytes of comm_size
+        cp_buff_size = buff_size;
+    } else {
+        fc_buff_size = comm_size + 2 + 2; //2 bytes of nr_of_comms, 2 bytes of comm_size
+        cp_buff_size = comm_size;
+    }
+    char first_comm[fc_buff_size + 4]; //4 bytes of checksum
+    memset(first_comm, 'A', fc_buff_size);
+    memcpy(first_comm + 4, buffer, cp_buff_size);
+
+    first_comm[0] = (nr_of_comms & 0xFF);             // Low byte of nr_of_comms
+    first_comm[1] = (nr_of_comms >> 8) & 0xFF;       // High byte of nr_of_comms
+    first_comm[2] = (comm_size & 0xFF);               // Low byte of comm_size
+    first_comm[3] = (comm_size >> 8) & 0xFF;         // High byte of comm_size
+
+    unsigned long checksum = crc32(0L, (const unsigned char *)first_comm, fc_buff_size);
+    first_comm[fc_buff_size - 5] = checksum;
+
+    // ---- DEBUGGING STEP ----
+    uint16_t received_nr_of_comms = (first_comm[0] | (first_comm[1] << 8)); // Combine bytes
+    printf("nr_of_comms: %d\n", received_nr_of_comms); // Cast to int for printing
+    uint16_t received_comm_size = (first_comm[2] | (first_comm[3] << 8)); // Combine bytes
+    printf("comm_size: %d\n", received_comm_size); // Cast to int for printing
+
+    // ---- FIRST COMMUNICATION ----
+
+    if nrofcomms == 1 {
+        write read
+    } else {
+        forloop {
+            make message - nrofcomms and commsize
+            write read
+        }
+    }
+
+    // ENVÍA UN MENSAJE AL SOCKET
+    n = write(sockfd, first_comm, strlen(first_comm));
+    if (n < 0) 
+            error("ERROR writing to socket");
+
+    // ESPERA RECIBIR UNA RESPUESTA
+    n = read(sockfd, first_comm, fc_buff_size + 4);
+    if (n < 0) 
+            error("ERROR reading from socket");
+
+    // ---- CIERRE COMUNICACION ----
+    printf("%s\n", first_comm);
+
+
+    // ---- INICIO COMM LOOP ----
+    for (int i = 0; i < nr_of_comms - 1; i++) {
+
+        // ENVÍA UN MENSAJE AL SOCKET
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0) 
+                error("ERROR writing to socket");
+
+        bzero(buffer, buff_size);
+
+        // ESPERA RECIBIR UNA RESPUESTA
+        n = read(sockfd, buffer, buff_size);
+        if (n < 0) 
+                error("ERROR reading from socket");
+
+        // ---- CIERRE COMUNICACION ----
+        printf("%s\n", buffer);
+    }
+
+    // CIERRA EL SOCKET
+    close(sockfd);
+
+    return 0;
+}
---
+++ practica1_ej2b/server.c
@@ -0,0 +1,98 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>   // Para exit, atoi y otras funciones estándar
+#include <string.h>   // Para bzero y otras funciones de cadenas
+#include <unistd.h>   // Para read, write y otras funciones de E/S
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <math.h>
+
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    
+    // CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXIÓN
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+    // SOCK_STREAM - TIPO DE SOCKET 
+    
+    if (sockfd < 0) 
+    error("ERROR opening socket");
+
+    // LIMPIA LA ESTRUCTURA serv_addr
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    
+    // ASIGNA EL PUERTO PASADO POR ARGUMENTO
+    // ASIGNA LA IP EN DONDE ESCUCHA (SU PROPIA IP)
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    // ENABLE SOCKET REUSE FOR SHELLSCRIPT
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        error("ERROR on setsockopt");
+    }
+
+    // VINCULA EL FILE DESCRIPTOR CON LA DIRECCIÓN Y EL PUERTO
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+            sizeof(serv_addr)) < 0) 
+            error("ERROR on binding");
+            
+    // SETEA LA CANTIDAD QUE PUEDEN ESPERAR MIENTRAS SE MANEJA UNA CONEXIÓN
+    listen(sockfd, 5);
+
+    // SE BLOQUEA A ESPERAR UNA CONEXIÓN
+    clilen = sizeof(cli_addr);
+
+    // ---- CONNECTION LOOP ----
+    for (int i = 1; i < 7; i++) {
+        newsockfd = accept(sockfd, 
+                    (struct sockaddr *) &cli_addr, 
+                    &clilen);
+                    
+        // DEVUELVE UN NUEVO DESCRIPTOR POR EL CUAL SE VAN A REALIZAR LAS COMUNICACIONES
+        if (newsockfd < 0) 
+            error("ERROR on accept");
+        
+        // ---- MENSAJE ----
+        int buff_size = pow(10,i);
+        char buffer[buff_size];
+        bzero(buffer, buff_size);
+
+        // LEE EL MENSAJE DEL CLIENTES
+        n = read(newsockfd, buffer, buff_size);
+        if (n < 0) error("ERROR reading from socket");
+        printf("Read %d bytes of buffer from fd\n",n);
+        //printf("Read %d bytes, here is the message: %s\n",n,buffer);
+
+        // RESPONDE AL CLIENTE
+        n = write(newsockfd, "I got your message", 18);
+        if (n < 0) error("ERROR writing to socket");
+
+        // CIERRA LOS SOCKETS
+        close(newsockfd);
+    }
+    
+    close(sockfd);
+
+    return 0; 
+}
