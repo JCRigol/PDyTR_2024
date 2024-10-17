3ff892733dbe9818c4ee51f2741f92b9cc553b10
JCRigol <juan.rigol.uni@gmail.com>
2024-10-17 08:11:55 -0300

    Add files via upload

--- practica2/correcciones/client.c
+++ practica2/correcciones/client.c
@@ -1,1 +1,177 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>  // Para strlen y otras funciones de cadenas
+#include <unistd.h>  // Para read, write y otras funciones de E/S
+#include <stdlib.h>  // Para exit y otras funciones estándar
+#include <math.h>
+#include <zlib.h> // For CRC32
+#include <stdbool.h>
 
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+double calculate_time(struct timeval start, struct timeval end, int repetitions)
+{
+    return ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1e6) / repetitions;
+}
+
+void timelog(int run_number, int buffer_size, double comm_time)
+{
+    FILE *fp;
+    fp = fopen("comm_times.csv", "a");
+    if (fp == NULL)
+        error("Failed to open csv file");
+
+    fprintf(fp, "%d,%d,%.6f\n", run_number, buffer_size, comm_time);
+    fclose(fp);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    if (argc < 4) {
+       fprintf(stderr,"usage %s hostname port potencia\n", argv[0]);
+       exit(1);
+    }
+
+    // TOMA EL NÚMERO DE PUERTO DE LOS ARGUMENTOS
+    portno = atoi(argv[2]);
+	
+    // CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXIÓN
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+    // SOCK_STREAM - TIPO DE SOCKET 
+	
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+	
+    // TOMA LA DIRECCIÓN DEL SERVIDOR DE LOS ARGUMENTOS
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    // LIMPIA LA ESTRUCTURA serv_addr
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+	
+    // COPIA LA DIRECCIÓN IP Y EL PUERTO DEL SERVIDOR A LA ESTRUCTURA DEL SOCKET
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+	
+    // DESCRIPTOR - DIRECCIÓN - TAMAÑO DIRECCIÓN
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+
+    // ---- DATA TO SEND ----
+    int potencia = atoi(argv[3]);
+    int buff_size = pow(10,potencia);
+    char buffer[buff_size];
+    memset(buffer, 'A', buff_size);
+    buffer[buff_size - 1] = '\0';  // Null-terminate the buffer for printf
+
+    // ---- UTILITIES ----
+    int nr_bytes_sent, ack_buff_size = 0;
+    unsigned long checksum;
+    bool corrupted_comm;
+    struct timeval start, end;
+    int amount_of_msgs_needed;
+
+    // ---- COMM START ----
+    for (int i = 0; i < 100; i++){
+        nr_bytes_sent = 0;
+        checksum = crc32(0L, (const unsigned char *)buffer, buff_size);
+        amount_of_msgs_needed = 0;
+
+        // ---- START COMM TIME ----
+        gettimeofday(&start, NULL);
+
+        // ---- FIRST COMMUNICATION (SEND TOTAL BUFFER SIZE, AWAIT ACK) ----
+        n = write(sockfd, &buff_size, sizeof(buff_size));
+        if (n < 0)
+            error("ERROR writing to socket");
+
+        n = read(sockfd, &ack_buff_size, sizeof(buff_size));
+        if (n < 0)
+            error("ERROR reading from socket");
+
+        amount_of_msgs_needed++;
+        printf("%d\n", ack_buff_size);
+
+        do {
+            // ---- BUFFER COMM LOOP ----
+            do {
+                n = write (sockfd, buffer + nr_bytes_sent, buff_size - nr_bytes_sent);
+                if (n < 0)
+                    error("ERROR writing to socket");
+
+                nr_bytes_sent += n;
+
+                // FANCY VERIFICATION
+                int bytes_received = 0, bytes_torecv = buff_size;
+                char ack_buffer[buff_size];
+                bzero(ack_buffer, buff_size);
+
+                do {
+                    n = read(sockfd, ack_buffer + bytes_received, bytes_torecv - bytes_received);
+                    if (n < 0)
+                        error("ERROR reading from socket");
+                    else if (n == 0)
+                        break;
+
+                    bytes_received += n;
+                    printf("Bytes received so far: %d\n", bytes_received);
+
+                    } while (n > 0 && bytes_received < bytes_torecv);
+
+                amount_of_msgs_needed++;
+
+                if(memcmp(buffer, ack_buffer, buff_size) == 0) {
+                    printf("Ack successfull");
+                } else {
+                    error("Ack buffer != buffer");
+                }
+
+            } while (nr_bytes_sent < buff_size);
+
+            // ---- VERIFICATION PHASE ----
+            n = write(sockfd, &checksum, sizeof(checksum));
+            if (n < 0)
+                error("ERROR writing to socket");
+
+            n = read(sockfd, &corrupted_comm, sizeof(bool));
+            if (n < 0)
+                error("ERROR reading from socket");
+
+            amount_of_msgs_needed++;
+
+            if (corrupted_comm)
+                nr_bytes_sent = 0;
+
+        } while (corrupted_comm);
+
+        // ---- END COMM TIME
+        gettimeofday(&end, NULL);
+
+        // ---- COMM END ----
+        timelog(i + 1, buff_size, calculate_time(start, end, amount_of_msgs_needed * 2));
+    } 
+
+    // CIERRA EL SOCKET
+    close(sockfd);
+
+    return 0;
+}
---
+++ practica2/correcciones/server.c
@@ -0,0 +1,156 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>   // Para exit, atoi y otras funciones estándar
+#include <string.h>   // Para bzero y otras funciones de cadenas
+#include <unistd.h>   // Para read, write y otras funciones de E/S
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <math.h>
+#include <zlib.h> // For CRC32
+#include <stdbool.h>
+
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    
+    // CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXIÓN
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+    // SOCK_STREAM - TIPO DE SOCKET 
+    
+    if (sockfd < 0) 
+    error("ERROR opening socket");
+
+    // LIMPIA LA ESTRUCTURA serv_addr
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    
+    // ASIGNA EL PUERTO PASADO POR ARGUMENTO
+    // ASIGNA LA IP EN DONDE ESCUCHA (SU PROPIA IP)
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    // ENABLE SOCKET REUSE FOR SHELLSCRIPT
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        error("ERROR on setsockopt");
+    }
+
+    // VINCULA EL FILE DESCRIPTOR CON LA DIRECCIÓN Y EL PUERTO
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+            sizeof(serv_addr)) < 0) 
+            error("ERROR on binding");
+            
+    // SETEA LA CANTIDAD QUE PUEDEN ESPERAR MIENTRAS SE MANEJA UNA CONEXIÓN
+    listen(sockfd, 5);
+
+    // SE BLOQUEA A ESPERAR UNA CONEXIÓN
+    clilen = sizeof(cli_addr);
+
+    // ---- CONNECTION LOOP ----
+    const char *ack_msg = "I got your message";
+
+    for (int i = 1; i < 7; i++) {
+        newsockfd = accept(sockfd, 
+                    (struct sockaddr *) &cli_addr, 
+                    &clilen);
+                    
+        // DEVUELVE UN NUEVO DESCRIPTOR POR EL CUAL SE VAN A REALIZAR LAS COMUNICACIONES
+        if (newsockfd < 0) 
+            error("ERROR on accept");
+
+        // ---- INITIAL CONFIG MESSAGE ----
+        int bytes_torecv;
+        unsigned long checksum, checksum_recvd;
+        bool checksum_failed;
+
+        // ---- 100 TIMES FOR TIME TESTING ----
+        for (int j = 0; j < 100; j++) {
+            n = read(newsockfd, &bytes_torecv, sizeof(int));
+            if (n < 0)
+                error("ERROR reading from socket");
+
+            printf("Bytes to recieve: %d\n",bytes_torecv);
+            char buffer[bytes_torecv];
+            bzero(buffer, bytes_torecv);
+
+            n = write(newsockfd, &bytes_torecv, sizeof(int));
+            if (n < 0)
+                error("ERROR writing to socket");
+
+            do {
+                // ---- COMM LOOP ----
+                int bytes_received = 0, outside_read_loops = 0;
+
+                do {
+                    int inside_read_loops = 0;
+
+                    // ---- COMMS READ LOOP ----
+                    do {
+                        n = read(newsockfd, buffer + bytes_received, bytes_torecv - bytes_received);
+                        if (n < 0)
+                            error("ERROR reading from socket");
+                        else if (n == 0)
+                            break;
+
+                        bytes_received += n;
+
+                        inside_read_loops++;
+                        printf("Bytes received so far: %d in %d inside read loops\n", bytes_received, inside_read_loops);
+
+                    } while (n > 0 && bytes_received < bytes_torecv);
+
+                    n = write(newsockfd, buffer, bytes_received);
+                    if (n < 0)
+                        error("ERROR writing to socket");
+
+                    outside_read_loops++;
+                    printf("Bytes received so far: %d in %d inside read loops, %d outside ones\n", bytes_received, inside_read_loops, outside_read_loops);          
+
+                } while (bytes_received < bytes_torecv);
+                
+                // ---- VERIFICATION ----
+                n = read(newsockfd, &checksum_recvd, sizeof(unsigned long));
+                if (n < 0)
+                    error("ERROR reading from socket");
+
+                checksum = crc32(0L, (const unsigned char *)buffer, bytes_received);
+                printf("Checksum received: %ld\nChecksum calculated: %ld\n", checksum_recvd, checksum);
+
+                checksum_failed = (checksum != checksum_recvd);
+
+                if (checksum_failed)
+                    bzero(buffer, bytes_torecv);
+
+                n = write(newsockfd, &checksum_failed, sizeof(checksum_failed));
+                if (n < 0)
+                    error("ERROR writing to socket");
+
+            } while(checksum_failed);
+        }
+
+        close(newsockfd);
+    }
+    
+    close(sockfd);
+
+    return 0; 
+}
